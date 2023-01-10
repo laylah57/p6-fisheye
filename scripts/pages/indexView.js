96c34268a1cb3ecc3d6047a0158a0448bbb706a7
Laure-Anne Jung <laureanne.vincent@gmail.com>
2023-01-10 11:54:45 +0100

    fix : filter style and behaviour

--- scripts/pages/indexView.js
+++ scripts/pages/indexView.js
@@ -11,18 +11,17 @@ class View {
 
   showPhotographer(photographer) {
     let html = `
-    <!--TODO : onclick + redirect to corresponding page -->
-    <a href="photographer.html?id=${photographer.id}" class="stretched-link">
       <article>
+        <a href="photographer.html?id=${photographer.id}" class="stretched-link">
           <div class="photographer_thumbnail">
-            <img alt="Portrait du photographer" src="/assets/photographers/${photographer.portrait}" />
+            <img alt="" src="/assets/photographers/${photographer.portrait}" />
           </div>
           <h2>${photographer.name}</h2>
-          <span class="location">${photographer.city + ', ' +  photographer.country}</span>
-          <span class="tagline">${photographer.tagline}</span>
-          <span class="price">${photographer.price + '€/jour'}</span>
+        </a>
+        <span class="location">${photographer.city + ', ' +  photographer.country}</span>
+        <span class="tagline">${photographer.tagline}</span>
+        <span class="price">${photographer.price + '€/jour'}</span>
       </article>
-    </a>
     `
     return html;
   }
